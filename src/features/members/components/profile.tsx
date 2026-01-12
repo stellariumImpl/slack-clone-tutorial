7dfaa18d3ceb8dc58341256562b6ee96e74e2638
felix <felix@felixs-MacBook-Air.local>
2026-01-12 21:53:45 +0800

    UI update

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -234,7 +234,7 @@ export const Profile = ({ memberId, onClose }: ProfileProps) => {
             </div>
           ) : null}
 
-          <div className="px-6">
+          <div className="px-3">
             <Separator className="bg-gray-200" />
           </div>
 
